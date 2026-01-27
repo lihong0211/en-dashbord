de476adae6d9a79ac896583979a8fbe1c14916cb
lihong <lihong@doctorwork.com>
2026-01-27 23:10:19 +0800

    fix: 时间

--- src/request/index.ts
+++ src/request/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: '/',
-  timeout: 3000,
+  timeout: 30000, // 增加到30秒，避免复杂查询超时
   headers: { 'X-Custom-Header': 'foobar' },
 });
 
