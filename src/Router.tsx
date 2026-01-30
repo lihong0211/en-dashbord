7376f1ed745fc475cb653a3c2b5ae279b186a2f2
lihong <lihong@doctorwork.com>
2026-01-30 20:11:03 +0800

    fix: 子域名

--- src/Router.tsx
+++ src/Router.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     ],
   },
 ],{
-  basename: '/en'
+  basename: '/'
 });
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/en/',
+  base: '/',
   server: {
     port: 3389,
     // host: '0.0.0.0',
